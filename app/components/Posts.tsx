84227068ef5dd46ce6ab31ed50e2d8afcfdd92c7
Lawrence Markel <LarryM093@gmail.com>
2023-06-09 15:01:43 -0400

    margin X axis posts updated

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -6,7 +6,7 @@ export default function Posts() {
     const posts = getSortedPostsData()
 
   return (
-    <section className="text-center md:mt-6 mx-auto max-w-2xl">
+    <section className="text-center md:mt-6 mx-10 max-w-2xl">
       
         <h2 className="text-4xl font-bold dark:text-slate-200">Blog</h2>
         <ul className="w-full">
