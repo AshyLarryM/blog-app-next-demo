0289bb4c78f80b1a4438c5a269651f56b0cdf5be
Lawrence Markel <LarryM093@gmail.com>
2023-06-09 16:15:04 -0400

    Updated Margins

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -11,7 +11,7 @@ export default function ListItem({ post }: Props) {
   const formattedDate = getFormattedDate(date)
 
   return (
-    <li className="mt-4 text-2xl dark:text-white/90">
+    <li className="mt-4 text-2xl dark:text-white/90 mx-4 ">
       <Link className="underline hover:text-black/20 dark:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500" href={`/posts/${id}`}>{title}</Link>
       <br />
       <p className="text-sm mt-1">{formattedDate}</p>
--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,19 +1,17 @@
 import { getSortedPostsData } from "@/lib/posts"
 import ListItem from "./ListItem"
 
-
 export default function Posts() {
-    const posts = getSortedPostsData()
+  const posts = getSortedPostsData()
 
   return (
-    <section className="text-center md:mt-6 mx-10 max-w-2xl">
-      
-        <h2 className="text-4xl font-bold dark:text-slate-200">Blog</h2>
-        <ul className="w-full">
-            {posts.map(post => (
-                <ListItem key={post.id} post={post} />
-            ))}
-        </ul>
+    <section className="text-center md:mt-6 max-w-2xl mx-auto">
+      <h2 className="text-4xl font-bold dark:text-slate-200">Blog</h2>
+      <ul className="w-full">
+        {posts.map(post => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
     </section>
   )
 }
