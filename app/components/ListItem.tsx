6873b13d30fbcf8398d8aa8c056848db0f3f3ad7
Lawrence Markel <LarryM093@gmail.com>
2023-06-09 14:31:33 -0400

    Color Scheme Update

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -12,7 +12,7 @@ export default function ListItem({ post }: Props) {
 
   return (
     <li className="mt-4 text-2xl dark:text-white/90">
-      <Link className="underline hover:text-black/30 dark:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500" href={`/posts/${id}`}>{title}</Link>
+      <Link className="underline hover:text-black/20 dark:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500" href={`/posts/${id}`}>{title}</Link>
       <br />
       <p className="text-sm mt-1">{formattedDate}</p>
     </li>
